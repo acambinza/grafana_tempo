ffb2aa84bb8db38b1e6de2bc879cb7c21f217e58
Anselmo Jorge <acambinza@gmail.com>
2025-01-06 23:50:47 +0100

    docker compose updated

--- app_example/index.js
+++ app_example/index.js
@@ -1,65 +1,34 @@
-const express = require('express')
-
 const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
+const express = require('express');
+const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { Resource } = require('@opentelemetry/resources');
 
-const appTempo = express()
-
-// config o OLTP HTTP Exporter
+// Configurar o exportador OTLP para o Tempo
 const traceExporter = new OTLPTraceExporter({
-	url: 'http://localhost:3100/api/traces',
-})
+  url: 'http://localhost:4318/v1/traces',
+});
 
-// Habilitando logs de diagnóstico para debug (opcional)
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-
-// Configurando o SDK do Node.js
+// Inicializar o SDK OpenTelemetry
 const sdk = new NodeSDK({
   traceExporter,
-  instrumentations: [
-    new HttpInstrumentation(), // Instrumentação HTTP
-    new ExpressInstrumentation(), // Instrumentação Express (opcional, se você usar Express)
-  ],
+  instrumentations: [getNodeAutoInstrumentations()],
+  spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
   resource: new Resource({
-	SERVICE_NAME: 'app_tempo',
-	SERVICE_VERSION: '1.0.0'
+    'service.name': 'app-trace-test',
   }),
 });
 
-// Inicializando o SDK
-try {
-	sdk.start()
-}catch(e) {
-	console.log("erro sdk tempo", e)
-}
+sdk.start();
 
-// Lidando com o encerramento do processo
-process.on('SIGTERM', async () => {
-  try {
-    await sdk.shutdown();
-    console.log('OpenTelemetry SDK shut down gracefully');
-    process.exit(0);
-  } catch (error) {
-    console.error('Error shutting down OpenTelemetry SDK', error);
-    process.exit(1);
-  }
+// App Node.js com Express
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Tracing example with Grafana Tempo!');
 });
 
-
-
-// iniciado a app
-appTempo.get('/', async (req, res) => {
-	res.end("Up renning APP TEMPO");
-})
-appTempo.get('/api/data', (req, res) => {
-	setTimeout(() => {
-		res.send("Hello, Grafana Tempo com OLTP/HTTP!")
-	}, Math.random() * 2000);
-})
-
-
-appTempo.listen(3001, () => console.log("Servidor Node.js rodando em http://localhost:3001"))
+app.listen(3001, () => {
+  console.log('Node.js app listening on port 3001');
+});
